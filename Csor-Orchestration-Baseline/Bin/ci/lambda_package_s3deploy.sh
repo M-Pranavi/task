65a2c4d6812755932c6f6479db67277416f6f1fe
M-Pranavi <87325488+M-Pranavi@users.noreply.github.com>
2025-09-04 16:22:02 -0500

    Create lambda_package_s3deploy.sh

---
+++ Csor-Orchestration-Baseline/Bin/ci/lambda_package_s3deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o pipefail
+
+lambda_name=$1
+env=$2
+account=$3
+lambda_tag=$4
+
+if [[ ! -n $lambda_tag ]]; then
+  lambda_tag=latest
+fi
+
+echo "Using lambda tag $lambda_tag"
+
+WORKSPACE=$(pwd)
+cd ${WORKSPACE}/lambdas/src/${lambda_name}
+pip install --requirement requirements.txt --target package
+cd package
+zip -r ../${lambda_name}.zip .
+cd ..
+zip ${lambda_name}.zip *.py
+
+zip_hash=$(shasum -a 256 ${lambda_name}.zip | awk '{print $1}')
+
+echo -n "{\"tag\": \"${lambda_tag}\", \"hash\": \"${zip_hash}\"}" > version.json
+
+echo "Lambda package ${lambda_name}.zip created successfully."
